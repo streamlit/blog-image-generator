a0418ae23a2b3cd24c10416b460662c69780ecc6
Juan Martin Garcia <juan.garcia@snowflake.com>
2022-08-23 13:09:30 -0300

    Better styles for announcement template

--- steps/step2_announcement.py
+++ steps/step2_announcement.py
@@ -4,7 +4,7 @@ from PIL import Image
 import io
 
 def render():
-    image = st.file_uploader("Choose an image", help="Recommended size: 1300x520 pixels")
+    image = st.file_uploader("Choose an image", help="Recommended size: 1300x540 pixels")
     return [image]
 
 
@@ -12,7 +12,7 @@ def generate(image):
     verify_arguments(image)
     
     # Get image byte data, resize and generate the base64 encoded version
-    buffered = resize_image(image, 1290, 520)
+    buffered = resize_image(image, 1290, 540)
     image = generate_base64_image(buffered.getvalue())
 
     # Generate all images
@@ -23,23 +23,55 @@ def generate(image):
         
         generated_images.append(f"""
             <svg width="100%" height="100%" viewBox="0 0 1480 700" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                # Gradient
-                <rect width="1480" height="700" fill="url(#gradient)"/>
 
-                # Image path
-                <image id="screenshot" x="95" y="180" width="1290" height="520" xlink:href="data:image/jpeg;charset=utf-8;base64,{image}" />
+                # Group
+                <g clip-path="url(#clip0_416_265)">
+                    
+                    # White background
+                    <rect width="1480" height="700" fill="white"/>
+                    
+                    # Gradient
+                    <rect width="1480" height="700" fill="url(#gradient)"/>
 
-                # White Background
-                <path xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" d="M76 64C69.3726 64 64 69.3726 64 76V700H96V188C96 183.582 99.5817 180 104 180H1376C1380.42 180 1384 183.582 1384 188V700H1416V76C1416 69.3726 1410.63 64 1404 64H76Z" fill="white"/>
+                    # Image path
+                    <image id="screenshot" x="95" y="160" width="1290" height="540" xlink:href="data:image/jpeg;charset=utf-8;base64,{image}" />
 
-                # Circles
-                <circle cx="106" cy="122" r="10" fill="#FF6C6C"/>
-                <circle cx="146.8" cy="122" r="10" fill="#FFE312"/>
-                <circle cx="187.6" cy="122" r="10" fill="#3DD56D"/>
+                    # Browser styles
+                    <g filter="url(#filter0_dd_416_265)">
+                        # Background
+                        <path fill-rule="evenodd" clip-rule="evenodd" d="M76 64C69.3726 64 64 69.3726 64 76V701H1416V76C1416 69.3726 1410.63 64 1404 64H76ZM104 162C99.5817 162 96 165.582 96 170V701H1384V170C1384 165.582 1380.42 162 1376 162H104Z" fill="white"/>
+
+                        # Stroke
+                        <path d="M63.5 701V701.5H64H95.5H96H1384H1384.5H1416H1416.5V701V76C1416.5 69.0964 1410.9 63.5 1404 63.5H76C69.0964 63.5 63.5 69.0964 63.5 76V701ZM1383.5 700.5H96.5V170C96.5 165.858 99.8579 162.5 104 162.5H1376C1380.14 162.5 1383.5 165.858 1383.5 170V700.5Z" stroke="#FAFAFA"/>
+                    </g>
+
+                    # Circles
+                    <circle cx="106" cy="122" r="10" fill="#FF6C6C"/>
+                    <circle cx="147" cy="122" r="10" fill="#FFE312"/>
+                    <circle cx="188" cy="122" r="10" fill="#3DD56D"/>
+                </g>
 
                 # Definitions
                 <defs>
                     
+                    # Filters
+                    <filter id="filter0_dd_416_265" x="24" y="29" width="1432" height="716" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                        <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+                        <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+                        <feOffset dy="2"/>
+                        <feGaussianBlur stdDeviation="6"/>
+                        <feComposite in2="hardAlpha" operator="out"/>
+                        <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0.258824 0 0 0 0 0.501961 0 0 0 0.05 0"/>
+                        <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_416_265"/>
+                        <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+                        <feOffset dy="5"/>
+                        <feGaussianBlur stdDeviation="20"/>
+                        <feComposite in2="hardAlpha" operator="out"/>
+                        <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0.258824 0 0 0 0 0.501961 0 0 0 0.25 0"/>
+                        <feBlend mode="normal" in2="effect1_dropShadow_416_265" result="effect2_dropShadow_416_265"/>
+                        <feBlend mode="normal" in="SourceGraphic" in2="effect2_dropShadow_416_265" result="shape"/>
+                    </filter>
+
                     # Image
                     <pattern id="pattern0" patternContentUnits="userSpaceOnUse" width="100%" height="100%">
                         <image id="screenshot" x="0" y="0" width="1290" height="520" xlink:href="data:image/jpeg;charset=utf-8;base64,{image}" />
@@ -47,6 +79,11 @@ def generate(image):
 
                     # Gradient
                     <linearGradient id="gradient" x1="" y1="0" x2="1" y2="0" gradientTransform="rotate(-45)">{gradients[i]}</linearGradient>
+
+                    # Browser clip path
+                    <clipPath id="clip0_416_265">
+                        <rect width="1480" height="700" fill="white"/>
+                    </clipPath>
                 </defs>
             </svg>
         """.strip())
