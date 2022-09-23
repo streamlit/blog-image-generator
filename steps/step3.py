49cde98383a2c306cc80e07b92568deee4d08f2a
Juan Martin Garcia <juan.garcia@snowflake.com>
2022-09-23 11:44:06 -0300

    Use template name as file name when downloading file

--- steps/step3.py
+++ steps/step3.py
@@ -1,11 +1,6 @@
 import streamlit as st
 import time as time
 import streamlit.components.v1 as components
-from cairosvg import svg2png
-
-@st.cache
-def to_png(svg_image):
-    return svg2png(bytestring=svg_image, output_width=1480, output_height=700)
 
 
 def display_output():
@@ -25,24 +20,12 @@ def display_output():
             </body>
         ''', height=333)
 
-        col1, col2 = st.columns(2)
-
-        with col1:
-            st.download_button(
-                label="Download SVG image",
-                data=svg_images[i],
-                file_name="image.svg",
-                mime="image/svg+xml"
-            )
-
-        with col2:
-            png_image = to_png(svg_images[i])
-            st.download_button(
-                label="Download PNG image",
-                data=png_image,
-                file_name="image.png",
-                mime="image/png"
-            )
+        st.download_button(
+            label="Download SVG image",
+            data=svg_images[i],
+            file_name=f'''{st.session_state.template_name}.svg''',
+            mime="image/svg+xml"
+        )
 
         st.write('')
         st.write('')
